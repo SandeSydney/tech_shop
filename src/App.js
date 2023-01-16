fdf75b40e1a59176a9aa499bedd57cdd225e28ce
SandeSydney <sandesydney@gmail.com>
2023-01-16 22:39:10 +0300

    Added static and dynamic content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation';
+import { router } from './routes/route_comp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <RouterProvider router={router} />
+    </>
   );
 }
 
