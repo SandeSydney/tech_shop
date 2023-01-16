bd512cc489b32a546e516ad4aa23931f38ea6ff2
SandeSydney <sandesydney@gmail.com>
2023-01-16 22:40:18 +0300

    Added and modified navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="" alt="TechShop Mlolongo" />
+        <div className='nav-items'>
+            <p>Home</p>
+            <p>Our Services</p>
+            <p>About Us</p>
+            <p>Contact Us</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
