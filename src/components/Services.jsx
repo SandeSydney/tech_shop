11b35548bc9bd4ade323283059eefed51da33870
SandeSydney <sandesydney@gmail.com>
2023-01-17 16:30:30 +0300

    Added services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <>
+            <div className='landing'>
+                <div className="main-container">
+                    <div className='service-content'>
+                        <h4 className="section-header">Our Services</h4>
+                        <p className='service-text'>
+                            We provide an extensive range of services. Your highest expectations will be fulfilled by our services. We create business solutions that are adaptable, scalable, cost-effective, and optimized. Your firm only needs the high-quality supplies and equipment we carry.
+                            We use the newest methods, most effective tools, digital trends, and a dash of originality to make your business venture a tremendous success.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="main-content"></div>
+        </>
+    )
+}
+
+export default Services
