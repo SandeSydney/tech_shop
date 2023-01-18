932a0967196d9400f4a62f8a37166f0ac8d1da16
SandeSydney <sandesydney@gmail.com>
2023-01-18 15:28:52 +0300

    Added about us section and route

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='main-container'>About</div>
+  )
+}
+
+export default About
--- src/routes/route_comp.jsx
+++ src/routes/route_comp.jsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom"
+import About from "../components/About"
+import ErrorPage from "../components/ErrorPage"
 import Homepage from "../components/Homepage"
 import Services from "../components/Services"
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Homepage />
+        element: <Homepage />,
+        errorElement: <ErrorPage />
     },
     {
         path: "services",
         element: <Services />
+    },
+    {
+        path: "about-us",
+        element: <About />
     }
 ])
